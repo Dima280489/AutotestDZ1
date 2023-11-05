a8c44a2e89f8eb12f09f9275a66485f0612c1137
Neto test <demon_4@mail.ru>
2023-11-05 15:47:20 +0300

    commt junit4platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -28,15 +28,4 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
-
-    @Test
-    public void testEqualThanMultiple() {
-        CashbackHackService cashbackService = new CashbackHackService();
-        int purchase = 1000;
-
-        int actual = cashbackService.remain(purchase);
-        int expected = 0;
-
-        assertEquals(expected, actual);
-    }
 }
